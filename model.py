ef62a1c60e53f889d9220eb74be55692c9cdc215
Patrick Chao <patrickrchao@gmail.com>
2018-01-11 16:49:48 -0500

    More model updates

--- model.py
+++ model.py
@@ -10,21 +10,22 @@ from torch.autograd import Variable
 import torch.nn as nn
 import torch.nn.functional as F
 
-model = models.vgg16(pretrained=True)
-model=nn.Sequential(*list(model.features.children())[:-1])
-print(model)
 
-
+#pool 2:   56 x 56 x 128
+#pool 3:   28 x 28 x 256
+#conv4_3:  28 x 28 x 512
+#conv5_3:  14 x 14 x 512
+#indices: 9,16,22,29
 class Model(nn.Module):
-    def __init__(self,pretrainedModel):
+    def __init__(self,pretrainedModel,indices):
         super(Model, self).__init__()
         self.model=pretrainedModel
+        self.indices = indices
 
-
-    def forward(self, x,indices):
+    def forward(self, x):
         output=[]
         for i in range(len(self.model)):
             x=self.model[i](x)
-            if i in indices:
+            if i in self.indices:
                 output += x
         return output
