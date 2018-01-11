b01395ca8a7281941529fab85cf16432ea1a2a27
Patrick Chao <patrickrchao@gmail.com>
2018-01-11 15:49:47 -0500

    Added requirements and base model.py file

---
+++ model.py
@@ -0,0 +1,13 @@
+import os
+import cv2
+import numpy as np
+import torch
+from torch.optim import SGD
+from torchvision import models,transforms, datasets
+from torch.autograd import Variable
+from torchvision import
+from skimage import io, transform
+import matplotlib.pyplot as plt
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.nn.functional as F
